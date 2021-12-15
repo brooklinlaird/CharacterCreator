ea1faf5c0eb5b9dd1c3354f08ec4883494719094
brooklinlaird <blaird2023@chadwickschool.org>
2021-12-15 00:08:18 -0800

    work

--- Character Creator/Assets/PlayerController.cs
+++ Character Creator/Assets/PlayerController.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public List<UnityEngine.UI.Button> WeaponButtons;
+    public List<UnityEngine.UI.Button> ArmorButtons; 
     public SpriteRenderer ArmorRenderer;
 
     public void SetArmor(Sprite newSprite)
@@ -15,6 +17,16 @@ public class PlayerController : MonoBehaviour
     public void SetWeapon(Sprite newSprite)
     {
         WeaponRenderer.sprite = newSprite;
+    }
+    public void SetRandomWeapon()
+    {
+        int randomNumber = Random.Range(0, 7);
+        this.WeaponButtons[randomNumber].onClick.Invoke();
+    }
+    public void  SetRandomArmor() 
+    {
+        int randomNumber = random range(0, 6);
+        this.ArmorButtons[randomnumber].onClick.Invoke();
     }
     // start this before the first frame
     void Start()
